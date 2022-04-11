b8b1165ca18b0c920b024ffc1c3f3d117aacc20d
Chuan Wang <chuan.wang101@gmail.com>
2022-04-11 12:15:59 -0700

    update search functions to use geocode to search by coords instead

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,24 +7,37 @@ function validateEntry(e) {
     } else if (/^\d+$/.test(e)) { //regex to check if string is only numbers
         if (e.length === 5) {
             // all number and 5 digit, use getByPostal
-            getByPostal(e);
+            geocode(e).then(function (coord) {
+                getByCoord(coord);
+            });
+            //getByCoord(coord);
         } else {
             console.log("wrong number digit for zipcode");
         }
-    } else if (/^[A-Za-z\s]*$/.test(e)){ //regex to check if string is letters and spaces only
-        // replace spaces with underscore
-        e = e.split(" ").join("_");
-        console.log(e);
-        // try searching by city first
-        getByCity(e);
+    } else if ( e.includes(",")) {
+        var eArray = e.split(",");
+        if (eArray.length != 2) {
+            console.log("invalid city entry");
+        }
+        eArray[0] = eArray[0].trim();
+        eArray[1] = eArray[1].trim();
+        if (/^[A-Za-z\s]*$/.test(eArray[0]) && /^[A-Za-z\s]*$/.test(eArray[1])){ //regex to check if string is letters and spaces only
+            eArray[0] = eArray[0].split(" ").join("%20");
+            e = eArray.join("+");
+            geocode(e).then(function (coord) {
+                getByCoord(coord);
+            });
+        } else {
+            console.log("invalid city entry");
+        }
     } else {
-        console.log("invalid entry");
+        getByName(e);
     }
 }
 
-function getByPostal(postal) {
-    // fetch by postal code
-    fetch(`https://api.openbrewerydb.org/breweries?by_postal=${postal}`)
+function getByCoord(coord) {
+        // fetch by coordinate sort by distance
+        fetch(`https://api.openbrewerydb.org/breweries?by_dist=${coord.lat},${coord.lng}`)
         .then(function (response) {
             if (!response.ok) {
                 //make sure response is ok, throw error if not
@@ -34,29 +47,6 @@ function getByPostal(postal) {
         })
         .then(function (data) {
             console.log(data);
-        })
-        .catch(function(error) {
-            console.log('error');
-        });
-}
-
-function getByCity(city) {
-    // fetch by city name
-    fetch(`https://api.openbrewerydb.org/breweries?by_city=${city}`)
-        .then(function (response) {
-            if (!response.ok) {
-                //make sure response is ok, throw error if not
-                throw new Error('response not ok');
-            }
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            if (data.length === 0) {
-                // try searching by name;
-                console.log("maybe not a city let's try search by name");
-                getByName(city);
-            }
         })
         .catch(function(error) {
             console.log('error');
@@ -64,8 +54,52 @@ function getByCity(city) {
 }
 
 function getByName(name) {
-    // TODO: code for search by name
+    // fetch by name
     console.log("searching by name");
+    fetch(`https://api.openbrewerydb.org/breweries?by_name=${name}`)
+        .then(function (response) {
+            if(!response.ok) {
+                throw new Error('response not ok');
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            if (data.length === 0) {
+                console.log('no results by name')
+            }
+        })
+        .catch(function(error) {
+            console.long('error')
+        })
+}
+
+function geocode(location) {
+    // location is zip code ex. 98005
+    // OR city + state encoded ex. san%20diego+ca
+    let url = `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyA-pYFi70-5pv6ldc1jAStH871OgfoMre8&address=${location}`;
+    return fetch(url)
+        .then(res => {
+            return res.ok ? res.json() : Promise.reject(res);
+        })
+        .then(res => {
+            if (res && res.results && res.results.length) {
+                return res.results[0].geometry.location;
+            } else {
+                Promise.reject(res);
+            }
+        })
+        .catch(err => {
+            console.log("err: ", err);
+        });
+}
+
+function showResults(data) {
+    if (data.length === 0) {
+        //TODO: show no results on html
+    } else {
+
+    }
 }
 
 $("#searchBtn").on('click', function (event) {
